58d7053afaf4f8c62925ad7c110f9f3606c6f031
Darshae <dallasvalderama@gmail.com>
2023-11-10 22:33:03 +0800

    test6

--- public/index.js
+++ public/index.js
@@ -32,8 +32,7 @@ const handleUIError = () => {
     errorBox.classList.add(classes.fadeIn);
     container.classList.remove('xl:h-[58rem]');
     container.classList.remove('h-[52rem]');
-    container.classList.add('h-60');
-    return
+    container.classList.add('h-[3rem]');
 };
 
 const handleUISuccess = () => {
